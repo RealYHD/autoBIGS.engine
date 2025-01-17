7ea7ead46aa787e3ae3a7e7c29822194424923aa
Harrison Deng <yunyangdeng@outlook.com>
2025-01-17 14:27:25 +0000

    Moved ABIF code to separate project

--- tests/automlst/engine/local/test_abif.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from automlst.engine.data.local.abif import read_abif
-
-async def test_load_sanger_sequence_has_data():
-    assert os.path.exists("tests/resources/1I1_F_P1815443_047.ab1")
-    result_data = await read_abif("tests/resources/1I1_F_P1815443_047.ab1")
-    assert result_data is not None
